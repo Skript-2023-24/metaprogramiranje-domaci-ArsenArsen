d09332cfbbda4a0b66ca037210f1b176d018f7e5
Arsen Arsenović <arsen@aarsen.me>
2023-11-11 11:56:20 +0100

    use solargraph

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((ruby-mode . ((eglot-server-programs . ((ruby-mode "/home/arsen/.local/share/gem/ruby/3.2.0/gems/ruby-lsp-0.12.3/exe/ruby-lsp"))))))
+((ruby-mode . ((eglot-server-programs . ((ruby-mode "solargraph"
+						    "stdio"))))))
